260c1943ffa3792ff7b0a8f560ac4383781ce325
scoony <scoonydeus@gmail.com>
2018-02-04 07:05:34 +0100

    Update yggtorrent.c.1h.sh

--- yggtorrent.c.1h.sh
+++ yggtorrent.c.1h.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version="0.0.0.4"
+version="0.0.0.5"
 
 #### Nettoyage
 ## Ne fonctionne pas
@@ -109,6 +109,7 @@ if [[ ! -f "$icons_cache/credits.png" ]] ; then curl -o "$icons_cache/credits.pn
 if [[ ! -f "$icons_cache/url.png" ]] ; then curl -o "$icons_cache/url.png" "https://raw.githubusercontent.com/scoony/yggtorrent.c.1h.sh/master/.cache-icons/url.png" ; fi
 if [[ ! -f "$icons_cache/vpn.png" ]] ; then curl -o "$icons_cache/vpn.png" "https://raw.githubusercontent.com/scoony/yggtorrent.c.1h.sh/master/.cache-icons/vpn.png" ; fi
 if [[ ! -f "$icons_cache/unprotected.png" ]] ; then curl -o "$icons_cache/unprotected.png" "https://raw.githubusercontent.com/scoony/yggtorrent.c.1h.sh/master/.cache-icons/unprotected.png" ; fi
+if [[ ! -f "$icons_cache/message.png" ]] ; then curl -o "$icons_cache/message.png" "https://raw.githubusercontent.com/scoony/yggtorrent.c.1h.sh/master/.cache-icons/message.png" ; fi
 
 #### Mise en variable des icones
 YGGTORRENT_ICON=$(curl -s "file://$icons_cache/yggtorrent.png" | base64 -w 0)
@@ -121,11 +122,13 @@ CREDITS_ICON=$(curl -s "file://$icons_cache/credits.png" | base64 -w 0)
 URL_ICON=$(curl -s "file://$icons_cache/url.png" | base64 -w 0)
 VPN_ICON=$(curl -s "file://$icons_cache/vpn.png" | base64 -w 0)
 UNPROTECTED_ICON=$(curl -s "file://$icons_cache/unprotected.png" | base64 -w 0)
+MESSAGE_ICON=$(curl -s "file://$icons_cache/message.png" | base64 -w 0)
 
 #### Récupération des informations de YGG
 ygg_login=`cat $HOME/.config/argos/.yggtorrent-account | awk '{print $1}'`
 ygg_password=`cat $HOME/.config/argos/.yggtorrent-account | awk '{print $2}'`
 website_main_url="https://yggtorrent.com"
+forum_url="https://forum.yggtorrent.com"
 
 #### Vérification de l'URL (en cas de changement)
 if [[ ! -f "$HOME/.config/argos/yggtorrent/.website_url.conf" ]]; then
@@ -176,12 +179,14 @@ humanise() {
 #### Récupération des détails du compte
 wget -q --load-cookies=$HOME/.config/argos/yggtorrent/cookies.txt "$website_url" -O $HOME/.config/argos/yggtorrent/page.html
 mon_ratio=`cat $HOME/.config/argos/yggtorrent/page.html | grep Ratio | sed 's/.*Ratio \: //' | sed 's/<\/a>.*//'`
-mon_upload=`cat $HOME/.config/argos/yggtorrent/page.html | grep fa-arrow-up | sed 's/.*;">//' | sed 's/<\/span>.*//' | sed 's/ //g'`
-mon_download=`cat $HOME/.config/argos/yggtorrent/page.html | grep fa-arrow-down | sed 's/.*;">//' | sed 's/<\/span>.*//' | sed 's/ //g'`
-mon_upload_detail=`dehumanise $mon_upload`
-mon_download_detail=`dehumanise $mon_download`
-mon_credit=$(($mon_upload_detail-$mon_download_detail))
-mon_credit_clair=`humanise $mon_credit`
+if [[ "$mon_ratio" != "" ]]; then
+  mon_upload=`cat $HOME/.config/argos/yggtorrent/page.html | grep fa-arrow-up | sed 's/.*;">//' | sed 's/<\/span>.*//' | sed 's/ //g'`
+  mon_download=`cat $HOME/.config/argos/yggtorrent/page.html | grep fa-arrow-down | sed 's/.*;">//' | sed 's/<\/span>.*//' | sed 's/ //g'`
+  mon_upload_detail=`dehumanise $mon_upload`
+  mon_download_detail=`dehumanise $mon_download`
+  mon_credit=$(($mon_upload_detail-$mon_download_detail))
+  mon_credit_clair=`humanise $mon_credit`
+fi
  
 #### Récupération de l'avatar du membre
 wget -q --load-cookies=$HOME/.config/argos/yggtorrent/cookies.txt "$website_url/user/account" -O $HOME/.config/argos/yggtorrent/page_account.html
@@ -199,9 +204,22 @@ else
   ip_status_icon=$UNPROTECTED_ICON
 fi
 
+#### Récupération de données sur le forum
+forum_login_page=`echo $forum_url"/index.php?app=core&module=global&section=login&do=process"`
+forum_login=`cat $HOME/.config/argos/.yggtorrent-account | awk '{print $3}'`
+forum_password=`cat $HOME/.config/argos/.yggtorrent-account | awk '{print $4}'`
+wget -q "$forum_url" -O "$HOME/.config/argos/yggtorrent/forum_page_key.html"
+forum_auth_key=`cat $HOME/.config/argos/yggtorrent/forum_page_key.html | grep "auth_key" | sed '/grep/d' | sed -n '1p' | grep -Po "(?<=value=')[^']*"`
+wget -q --save-cookies $HOME/.config/argos/yggtorrent/forum_cookies.txt --keep-session-cookies --post-data="auth_key=$forum_auth_key&referer=http%3A%2F%2Fforum.yggtorrent.com%2Findex.php&ips_username=$forum_login&ips_password=$forum_password&rememberMe=1" "$forum_login_page"
+wget -q --load-cookies=$HOME/.config/argos/yggtorrent/forum_cookies.txt "$forum_url" -O $HOME/.config/argos/yggtorrent/forum_page.html
+get_message_amount=`cat $HOME/.config/argos/yggtorrent/forum_page.html | grep "getInboxList" | sed '/grep/d' | grep -Po "(?<=ipsHasNotifications'>)[^<]*"`
+if [[ "$get_message_amount" == "" ]]; then
+  get_message_amount="0"
+fi
+
 #### Préparation des paramètres
-account_infos=`echo -e "zenity --forms --width=500 --window-icon=\"~/.config/argos/.cache-icons/yggtorrent.png\" --title=\"Authentification du compte\" --text=\"Veuillez entrer vos informations\" --add-entry=\"Identifiant YGG\" --add-entry=\"Mot de passe YGG\" --separator=\" \" 2>/dev/null >~/.config/argos/.yggtorrent-account"`
- 
+account_infos=`echo -e "yad --width=\"600\" --height=\"300\" --center --window-icon=\"$HOME/.config/argos/.cache-icons/yggtorrent-big.png\" --title=\"Authentification du compte\" --text=\"<big>\r\rVeuillez entrer vos informations didentification.\rCes informations ne transittent pas par internet.\r\r</big>\" --text-align=center --image=\"$HOME/.config/argos/.cache-icons/yggtorrent-big.png\" --form --separator=\" \" --field=\"Identifiant du site\" --field=\"Mot de passe du site\" --field=\"Identifiant du forum\" --field=\"Mot de passe du forum\" \"$ygg_login\" \"$ygg_password\" \"$forum_login\" \"$forum_password\" --button=gtk-ok:0 2>/dev/null >~/.config/argos/.yggtorrent-account"`
+
 #### On affice le résultat
 echo " $mon_credit_clair | image='$YGGTORRENT_ICON' imageWidth=25"
 echo "---"
@@ -214,5 +232,8 @@ printf "%-2s \e[1m%-20s\e[0m : %s | image='%s' imageWidth=18 ansi=true font='Ubu
 echo "---"
 printf "%-2s \e[1m%-20s\e[0m : %s | image='%s' imageWidth=18 ansi=true font='Ubuntu Mono' trim=false \n" "" "URL actuelle" "$website_url" "$URL_ICON"
 printf "%-2s \e[1m%-20s\e[0m : %s | image='%s' imageWidth=18 ansi=true font='Ubuntu Mono' trim=false \n" "" "Status de l'IP" "$ip_status" "$ip_status_icon"
+if [[ "$forum_login" != "" ]] && [[ "$forum_password" != "" ]]; then
+  printf "%-2s \e[1m%-20s\e[0m : %s | image='%s' imageWidth=18 ansi=true font='Ubuntu Mono' trim=false \n" "" "Messagerie Forum" "$get_message_amount message(s) non-lu(s)" "$MESSAGE_ICON"
+fi
 echo "---"
 printf "%-2s %s | image='$SETTINGS_ICON' imageWidth=18 ansi=true font='Ubuntu Mono' trim=false bash='$account_infos' terminal=false \n" "" "Paramètres du compte YGG"
